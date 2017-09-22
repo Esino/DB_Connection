018cdce868db7b07df8fdf0580208b6ae1e3708f
System Administrator <root@Pauls-MBP.default>
2017-09-22 16:00:30 +0100

    {Created a php prepared statement and bound parameters}

--- db_create.php
+++ db_create.php
@@ -10,36 +10,24 @@
 		die("Connection failed:" . $conn->connect_error);
 	}
 	
-	/* $sql = "CREATE TABLE MyGuests (
-		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-		firstname VARCHAR(30) NOT NULL,
-		lastname VARCHAR(30) NOT NULL,
-		email VARCHAR(50),
-		reg_date TIMESTAMP
-		)";
+	//prepare and bind
+	$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) 
+							VALUES (?, ?, ?)");
+	$stmt->bind_param("sss", $firstname, $lastname, $email);
 
-	if ($conn->query($sql) === TRUE) {
-	    echo "Table MyGuests created successfully";
-	} else {
-	    echo "Error creating table: " . $conn->error;
-	} */
+	// Set parameters and execute
+	$firstname = "Barkley";
+	$lastname = "Ford";
+	$email = "BarkFord@gmail.com";
+	$stmt->execute();
 
-	$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-			VALUES ('Seth', 'Meyers', 'Seth135@gmail.com');";
+	$firstname = "Anita";
+	$lastname = "Stone";
+	$email = "AniStone@gmail.com";
+	$stmt->execute();
 
-	$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
-			VALUES ('Blessing', 'Lewis', 'BlessLe@gmail.com');";
-
-	$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
-			VALUES ('Charlotte', 'Williams', 'char.williams@gmail.com')";
-
-
-	if ($conn->multi_query($sql) === TRUE) {
-		echo "New records created successfully.";
-	}
-	else{
-		echo "Error:". $ql . "<br>" . $conn->error;
-	}
+	echo "New records created successfully";
 		
+	$stmt->close();	
 	$conn->close();
 ?>
